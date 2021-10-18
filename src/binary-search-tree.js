d80056bbc6188e7315466c3adf45251a59c8d8eb
Parfionov Anton <toystix1992@gmail.com>
2021-10-18 21:44:50 +0300

    add finaly solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -18,75 +18,110 @@ module.exports = class BinarySearchTree {
 
   add(data) {
     const newNode = new Node(data);
-    if(!this.nodeRoot) {
+    if (!this.nodeRoot) {
       this.nodeRoot = newNode;
       return this.nodeRoot;
     }
     let currentNode = this.nodeRoot;
     while (currentNode) {
-      if(currentNode.data === newNode.data) {
+      if (currentNode.data === newNode.data) {
         return;
       }
-      if(newNode.data < currentNode.data) {
-        if(!currentNode.left){
+      if (newNode.data < currentNode.data) {
+        if (!currentNode.left) {
           currentNode.left = newNode;
           return;
+        }
+        currentNode = currentNode.left;
       }
-      currentNode = currentNode.left;
-      }
-      if(newNode.data > currentNode.data) {
-        if(!currentNode.right){
+      if (newNode.data > currentNode.data) {
+        if (!currentNode.right) {
           currentNode.right = newNode;
           return;
-      }
-      currentNode = currentNode.right;
+        }
+        currentNode = currentNode.right;
       }
     }
   }
 
   has(data) {
     let currentNode = this.nodeRoot;
-    while(currentNode){
-        if(data === currentNode.data) {
-          return true;
-        }
-        if(data < currentNode.data){
-          currentNode = currentNode.left;
-        } else {
-          currentNode = currentNode.right;
-        }
+    while (currentNode) {
+      if (data === currentNode.data) {
+        return true;
+      }
+      if (data < currentNode.data) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
     }
     return false;
   }
 
   find(data) {
-    if(!this.nodeRoot) {
+    if (!this.nodeRoot) {
       return null;
     }
     let currentNode = this.nodeRoot;
-    while(currentNode){
-    if(data === currentNode.data) {
-      return currentNode;
+    while (currentNode) {
+      if (data === currentNode.data) {
+        return currentNode;
+      }
+      if (data < currentNode.data) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
     }
-    if(data < currentNode.data){
-      currentNode = currentNode.left;
-    } else {
-      currentNode = currentNode.right;
-    }
-  }
     return null;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.nodeRoot = handlerRemove(this.nodeRoot, data);
+
+    function handlerRemove(node, value) {
+      if (node === null) {
+        return null;
+      }
+      if (value < node.data) {
+        node.left = handlerRemove(node.left, value);
+        return node;
+      }
+      if (value > node.data) {
+        node.right = handlerRemove(node.right, value);
+        return node;
+      }
+      if (value === node.data) {
+        if (!node.left && !node.right) {
+          return null;
+        }
+        if (node.left === null) {
+          node = node.right;
+          return node;
+        }
+        if (node.right === null) {
+          node = node.left;
+          return node;
+        }
+
+        let minFromRight = node.right;
+        while (minFromRight.left) {
+          minFromRight = minFromRight.left;
+        }
+        node.data = minFromRight.data;
+        node.right = handlerRemove(node.right, minFromRight.data)
+
+        return node;
+      }
+    }
   }
 
   min() {
     let currentNode = this.nodeRoot;
     let min = currentNode.data;
 
-    while(currentNode){
+    while (currentNode) {
       min = currentNode.data;
       currentNode = currentNode.left;
     }
@@ -97,7 +132,7 @@ module.exports = class BinarySearchTree {
   max() {
     let currentNode = this.nodeRoot;
     let max = currentNode.data;
-    while(currentNode){
+    while (currentNode) {
       max = currentNode.data;
       currentNode = currentNode.right;
     }
